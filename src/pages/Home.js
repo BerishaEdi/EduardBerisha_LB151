6119bf90287ced19d1672e11480fb5e7ed29720e
BerishaEdi <berisha.edi@outlook.com>
2023-02-26 13:45:20 +0100

    RandomWord

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { auth, db } from "../firebase";
 
 const Home = () => {
+  const [word, setWord] = useState("");
+  const [wordsArray, setWordsArray] = useState([]);
+
+  useEffect(() => {
+    loadWords();
+  }, [])
+
+  const loadWords = () => {
+    db.collection("words")
+      .get()
+      .then((querySnapshot) => {
+        const newWordsArray = [];
+        querySnapshot.forEach((doc) => {
+          newWordsArray.push({
+            word: doc.data().name,
+          });
+        });
+        setWordsArray(newWordsArray);
+        getRandomWord(newWordsArray);
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }
+
+  const getRandomWord = (array) => {
+    const shuffledArray = array.sort(() => Math.random() - 0.5);
+    const firstWord = shuffledArray[0].word;
+    setWord(firstWord);
+    console.log(firstWord);
+  }
+
   return (
-    <div>Home</div>
+    <div className='Home'>
+      <p>Home</p>
+      <p>Random Word: {word}</p>
+    </div>
   )
 }
+
 
 export default Home
